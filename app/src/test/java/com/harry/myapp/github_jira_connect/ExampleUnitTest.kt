91dede3824d5812ad892c731dc0a457c01c1e929
harry <harry7408@naver.com>
2025-08-08 12:52:28 +0900

    [Chore] : yaml 파일에 주석 추가

--- app/src/test/java/com/harry/myapp/github_jira_connect/ExampleUnitTest.kt
+++ app/src/test/java/com/harry/myapp/github_jira_connect/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.harry.myapp.github_jira_connect
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
